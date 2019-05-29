969f735839e489527f3cc6b1899c6c9d65747c58
JGYTVevo <36227813+JGYTVevo@users.noreply.github.com>
2019-05-29 17:32:51 +1200

    Add files via upload

---
+++ src/Cmd/Main.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Cmd;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+
+        $this->getServer()->getLogger("FHJG Enabled");
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+
+        switch($cmd->getName()){
+            case "f": 
+if($sender->hasPermission("feed.jg")){
+$sender->setFood(20);
+$sender->sendMessage("[CONSOLE] Has Feed Your Hunger!");
+} else {
+    $sender->sendMessage("[!] No permission please message staff if needed");
+}
+break;
+case "h": 
+if($sender->hasPermission("heal.jg")){
+$sender->setHealth(20);
+$sender->sendMessage("[CONSOLE] Has Healed You!");
+} else {
+    $sender->sendMessage("[!] No permission please message staff if needed");
+}
+break;
+        }
+return true;
+    }
+}
+
