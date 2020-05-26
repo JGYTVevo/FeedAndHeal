e7db4b50a408ccd843afe97d9a19d6016d3ed0b6
JGYTVevo <36227813+JGYTVevo@users.noreply.github.com>
2020-05-26 18:51:31 +1200

    think i fixed colours

--- src/JGYTVevo/FeedAndHeal/Main.php
+++ src/JGYTVevo/FeedAndHeal/Main.php
@@ -22,15 +22,15 @@ $sender->setFood(20);
 $sender->setSaturation(20);    
 $sender->sendMessage("You have been fed!");
 } else {
-    $sender->sendMessage(C:: RED. "Error: No permission!");
+    $sender->sendMessage(C::RED. "Error: No permission!");
 }
 break;
 case "heal": 
 if($sender->hasPermission("heal.jg")){
 $sender->setHealth(20);
-$sender->sendMessage(C:: GREEN. "You have been healed!");
+$sender->sendMessage(C::GREEN. "You have been healed!");
 } else {
-    $sender->sendMessage(C:: RED. "Error: No permission");
+    $sender->sendMessage(C::RED. "Error: No permission");
 }
 break;
         }
